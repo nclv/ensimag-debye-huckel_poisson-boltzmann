97e4dc3de2da3829db4fbb35f0a7df6db578a110
n0way <n0way@localhost.localdomain>
2020-05-09 10:06:22 +0200

    q8

--- lu.py
+++ lu.py
@@ -5,8 +5,10 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from pprint import pprint
 
-plt.style.use('ggplot')
+plt.style.use("ggplot")
+
 
 def lutri(a, b, c):
     """
@@ -22,7 +24,8 @@ def lutri(a, b, c):
     for i in range(1, n):
         l.append(b[i - 1] / v[i - 1])
         v.append(a[i] - l[i - 1] * c[i - 1])
-    return [l, v]    
+    return [l, v]
+
 
 def descente(l, z):
     """
@@ -38,6 +41,7 @@ def descente(l, z):
         y.append(z[i] - l[i - 1] * y[i - 1])
     return y
 
+
 def remontee(v, c, y):
     """
         Effectue l'étape de remontée lors de la factorisation LU d'une matrice A tridiagonale
@@ -52,6 +56,7 @@ def remontee(v, c, y):
     for i in range(n - 2, -1, -1):
         x.insert(0, (y[i] - c[i] * x[0]) / v[i])
     return x
+
 
 def solve_debye_huckel(n, mu):
     """
@@ -70,7 +75,8 @@ def solve_debye_huckel(n, mu):
         b[i - 1] = 1 - h / (2 * xi)
         c[i] = 1 + h / (2 * xi)
         x[i] = xi
-    xn = (1 + (n - 1) * h)
+    # Dernier élément
+    xn = 1 + (n - 1) * h
     b[n - 2] = 1 - h / (2 * xn)
     x[n - 1] = xn
 
@@ -78,22 +84,25 @@ def solve_debye_huckel(n, mu):
     y = descente(l, z)
     u = remontee(v, c, y)
 
-    return x, u
+    return x, np.array(u), [b, a, c]
+
 
 def plot_debye_huckel(x, u):
     plt.plot(x, u, label="u")
-    plt.xlabel('Points de discrétisations $x_i$')
-    plt.ylabel('Solutions ponctuelles $u_i$')
+    plt.xlabel("Points de discrétisations $x_i$")
+    plt.ylabel("Solutions ponctuelles $u_i$")
     plt.title("Schéma aux différences finies de l'équation de Debye-Huckel")
     plt.legend()
     plt.show()
+
 
 def plot_echantillon_q5():
     """
         Affichage des couples (xi, ui)
     """
-    x, u = solve_debye_huckel(n = 1000, mu = 1)
+    x, u, _ = solve_debye_huckel(n=1000, mu=1)
     plot_debye_huckel(x, u)
+
 
 def plot_echantillon_q6():
     """
@@ -101,17 +110,18 @@ def plot_echantillon_q6():
     """
     u0, h = [], []
     for n in [10, 15, 30, 70, 100, 1000, 10000]:
-        _, u = solve_debye_huckel(n, mu = 1)
+        _, u, _ = solve_debye_huckel(n, mu=1)
         # plot_debye_huckel(x, u)
         u0.append(u[0])
         h.append(10 / n)
     # print(h, u0)
-    plt.plot(h, u0, 'o', label="$u_0$")
-    plt.xlabel('Pas de discrétisations $h$')
-    plt.ylabel('Solutions ponctuelles $u_0$')
+    plt.plot(h, u0, "o", label="$u_0$")
+    plt.xlabel("Pas de discrétisations $h$")
+    plt.ylabel("Solutions ponctuelles $u_0$")
     plt.title("Influence du pas de la discrétisation")
     plt.legend()
     plt.show()
+
 
 def solve_poisson_boltzmann(u, n, mu):
     """
@@ -126,7 +136,7 @@ def solve_poisson_boltzmann(u, n, mu):
     # Initialisation
     x = [1] * n
     b, a, c, z = [0] * (n - 1), [-(2 + h ** 2)] * n, [0] * (n - 1), [0] * n
-    c[0], z[0] = 2,(h ** 2) * g(u[0]) + mu * h * (h - 2)
+    c[0], z[0] = 2, (h ** 2) * g(u[0]) + mu * h * (h - 2)
     # Attribution
     for i in range(1, n - 1):
         xi = 1 + i * h
@@ -135,7 +145,7 @@ def solve_poisson_boltzmann(u, n, mu):
         z[i] = (h ** 2) * g(u[i])
         x[i] = xi
     # Dernier élément
-    xn = (1 + (n - 1) * h)
+    xn = 1 + (n - 1) * h
     b[n - 2] = 1 - h / (2 * xn)
     x[n - 1] = xn
     z[n - 1] = (h ** 2) * g(u[n - 1])
@@ -144,18 +154,62 @@ def solve_poisson_boltzmann(u, n, mu):
     y = descente(l, z)
     u_suivant = remontee(v, c, y)
 
-    return x, u_suivant
+    return x, np.array(u_suivant), np.array(z)
+
+
+def tridiag(diags, k1=-1, k2=0, k3=1):
+    """Retourne une matrice tridiagonale."""
+    b, a, c = diags
+    return np.diag(b, k1) + np.diag(a, k2) + np.diag(c, k3)
+
+
+def calcul_uk0(mu = 1):
+    # Paramètres de la simualtion
+    mu1, mu2 = 10e-12, 10e-9
+    n = 1000
+
+    # Initialisation de u et calcul de A
+    _, u, diags = solve_debye_huckel(n, mu)
+    A = tridiag(diags)
+    # Initialisation des écarts
+    ecart1 = A.dot(u)
+    ecart2 = u
+
+    k = 0
+    while not (k > 200 or (np.linalg.norm(ecart1, np.inf) < mu1 and np.linalg.norm(ecart2, np.inf) < mu2)):
+        x, u_suivant, z = solve_poisson_boltzmann(u, n, mu)
+        ecart1 = A.dot(u_suivant) - z
+        ecart2 = u_suivant - u
+        u = u_suivant
+        k += 1
+
+    return x, u, k
+
+
+def plot_echantillon_q8(mu):
+    x, u, k = calcul_uk0(mu = mu)
+    print(f"k vaut {k} pour mu = {mu}")
+    plt.plot(x, u, label="u")
+    plt.xlabel("Points de discrétisations $x_i$")
+    plt.ylabel("Solutions ponctuelles $u_i$")
+    plt.title("Schéma aux différences finies de l'équation de Poisson-Boltzmann")
+    plt.legend()
+    plt.show()
+
 
 def calculer_u(u, mu, n):
     """
         Calcule u(k+1) en fonction de u(k)
     """
     h = 10 / n
-    z = [(h ** 2) * (np.sinh(u[0]) - u[0]) + mu * h * (h - 2)] + [(h ** 2) * (np.sinh(i) - i) for i in u[1:]]
+    z = [(h ** 2) * (np.sinh(u[0]) - u[0]) + mu * h * (h - 2)] + [
+        (h ** 2) * (np.sinh(i) - i) for i in u[1:]
+    ]
     u_suivant = solve_debye_huckel(n, mu)
     return u_suivant
 
-def plot_echantillon_q8(mu, n):
+
+def plot_echantillon_q8_b(mu, n):
     eta1 = 10 ** (-12)
     eta2 = 10 ** (-9)
     k = 1
@@ -163,23 +217,44 @@ def plot_echantillon_q8(mu, n):
     a = [-(2 + h ** 2) for _ in range(n)]
     b = [1 - h / (2 * (1 + i * h)) for i in range(1, n)]
     c = [2] + [1 + h / (2 * (1 + i * h)) for i in range(1, n - 1)]
-    z = [mu * h * (h - 2)] + [0 for _ in range(n - 1)] 
+    z = [mu * h * (h - 2)] + [0 for _ in range(n - 1)]
     [l, v] = lutri(a, b, c)
     y = descente(l, z)
-    u = remontee(v, c, y) #Calcul de u0 JUSQUA LA C'EST OK
+    u = remontee(v, c, y)  # Calcul de u0 JUSQUA LA C'EST OK
     tmp_u = u
     while True:
         u_suivant = calculer_u(tmp_u, mu, n)
         a_modifie = [elt1 * elt2 for elt1, elt2 in zip(a, u_suivant)]
-        b_modifie = [elt1 * elt2 for elt1, elt2 in zip(b, u_suivant[:len(u_suivant) - 1])]
-        c_modifie = [elt1 * elt2 for elt1, elt2 in zip(c, u_suivant[1:])] #JUSQUE LA CA SEMBLE OK
-        #print(a_modifie)
-        #print(c_modifie)
-        au = [a_modifie[0] + c_modifie[0]] + [elt1 + elt2 + elt3 for elt1, elt2, elt3 in zip(b_modifie[:len(b_modifie) - 1], a_modifie[1:len(a_modifie) - 1], c_modifie[1:])] + [b_modifie[-1] + a_modifie[-1]]
-        g = [(h ** 2) * (np.sinh(u_suivant[0]) - u_suivant[0]) + mu * h * (h - 2)] + [(h ** 2) * (np.sinh(i) - i) for i in u_suivant[1:]]
+        b_modifie = [
+            elt1 * elt2 for elt1, elt2 in zip(b, u_suivant[: len(u_suivant) - 1])
+        ]
+        c_modifie = [
+            elt1 * elt2 for elt1, elt2 in zip(c, u_suivant[1:])
+        ]  # JUSQUE LA CA SEMBLE OK
+        # print(a_modifie)
+        # print(c_modifie)
+        au = (
+            [a_modifie[0] + c_modifie[0]]
+            + [
+                elt1 + elt2 + elt3
+                for elt1, elt2, elt3 in zip(
+                    b_modifie[: len(b_modifie) - 1],
+                    a_modifie[1 : len(a_modifie) - 1],
+                    c_modifie[1:],
+                )
+            ]
+            + [b_modifie[-1] + a_modifie[-1]]
+        )
+        g = [(h ** 2) * (np.sinh(u_suivant[0]) - u_suivant[0]) + mu * h * (h - 2)] + [
+            (h ** 2) * (np.sinh(i) - i) for i in u_suivant[1:]
+        ]
         norme1 = [elt1 - elt2 for elt1, elt2 in zip(au, g)]
         norme2 = [elt1 - elt2 for elt1, elt2 in zip(u_suivant, tmp_u)]
-        if (k > 200 or np.linalg.norm(np.array(norme1)) < eta1 and np.linalg.norm(np.array(norme2)) < eta2):
+        if (
+            k > 200
+            or np.linalg.norm(np.array(norme1)) < eta1
+            and np.linalg.norm(np.array(norme2)) < eta2
+        ):
             print("K = {}".format(str(k)))
             print("Norme 1 = {}".format(str(np.linalg.norm(np.array(norme1)))))
             print("Norme 2 = {}".format(str(np.linalg.norm(np.array(norme2)))))
@@ -187,6 +262,7 @@ def plot_echantillon_q8(mu, n):
         k += 1
         tmp_u = u_suivant
     return k
+
 
 def main():
     b, a, c = [2, 6], [1, 10, 10], [4, 5]
@@ -199,6 +275,9 @@ def main():
 
     # plot_echantillon_q5()
     # plot_echantillon_q6()
+
+    plot_echantillon_q8(1)
+    plot_echantillon_q8(4)
 
     # b, a, c = [-4, -3, -2, 2], [-2, 5, -1, 4, -2], [1, 2, -1, 1]
     # l, v = lutri(a, b, c)
